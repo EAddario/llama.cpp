e6cd408720e3b196e22d9e2f45697134ba4e7bf7
Ed Addario <eaddario@hotmail.com>
2026-04-23 08:57:13 +0100

    Release finished tensors mmap allocation

--- src/llama-quant.cpp
+++ src/llama-quant.cpp
@@ -2549,6 +2549,10 @@ static void llama_model_quantize_impl(const std::string & fname_inp, const std::
             // write tensor data + padding
             fout.write((const char *) new_data, new_size);
             zeros(fout, GGML_PAD(new_size, align) - new_size);
+
+            if (ml.use_mmap) {
+                ml.mappings.at(weight.idx)->unmap_fragment(weight.offs, weight.offs + tensor_size);
+            }
         }
     }
 
